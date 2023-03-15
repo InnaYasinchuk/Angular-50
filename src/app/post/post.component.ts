8bdf61cf3669ef357b460cf0cd826ec4a77483eb
Inna Yasynchuk <i_bybyk@ukr.net>
2023-03-15 17:39:40 +0200

    solved the tasks

---
+++ src/app/post/post.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-post',
+  templateUrl: './post.component.html',
+  styleUrls: ['./post.component.scss'],
+})
+export class PostComponent {
+  title = 'High-Performance Computing';
+  text =
+    'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat, illo porrsit, incidunt fuga cum est sed labore repellendus eos veritatis vitae magni cupiditate. Adipisci officiis eius perferendis dolor odit.';
+}
